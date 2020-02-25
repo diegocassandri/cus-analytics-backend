9bbb061e05950505911cdd77173a585ac573661f
Diego Cassandri <diego.cassandri@gmail.com>
2020-02-25 20:44:48 -0300

    Error handling

--- src/index.js
+++ src/index.js
@@ -10,5 +10,21 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
+
+
 app.listen(process.env.PORT || 3000);
 
